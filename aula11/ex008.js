90eff5456feb1e251a8021b1e185f094d298fbb7
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-01-07 17:57:44 -0300

    concluídos os exercícios 008, 009, 010 e 011

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.1
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida! MULTADO!`)
+} // Condição Simples
+
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo no ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Nativo')
+} else {
+    console.log('Voce é Estrangeiro')
+} // Condição composta
