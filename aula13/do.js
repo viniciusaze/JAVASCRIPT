24bea4a2acfb4a8538d7e7fb30a852c18700f53b
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-01-11 16:06:09 -0300

    exercício do

---
+++ aula13/do.js
@@ -0,0 +1,7 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while( c <= 6 )
+
+//Teste lógico no final
---
+++ aula13/while.js
@@ -0,0 +1,7 @@
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+//Teste lógico no início
