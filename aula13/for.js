2954fd661064052ca8c08c87a40d94af7a507c88
viniciusaze <152533810+viniciusaze@users.noreply.github.com>
2024-01-11 17:30:37 -0300

    exercício resolvido

---
+++ aula13/for.js
@@ -0,0 +1,9 @@
+console.log('Vai começar...')
+for (var c = 1 ; c<=6 ; c++){
+    console.log(`Passo ${c}`)
+}
+console.log('FIM')
+
+
+
+// ESTRUTURA DE REPETIÇÃO COM VARIAVEL DE CONTROLE
---
+++ aula14ex/modelo/script.js
@@ -0,0 +1,36 @@
+function contar(){
+    var NumInicio = document.getElementById('iinicio')
+    var NumFinal = document.getElementById('ifim')
+    var passo = document.getElementById('ipasso')
+    var resultado = document.getElementById('resultado')
+
+    if (passo.value.length == 0 || NumInicio.value.length == 0 || NumFinal.value.length == 0 ){
+       alert('[ERRO] Faltam dados.')
+       resultado.innerHTML = `Impossível contar`
+
+    } else {
+        resultado.innerHTML = 'Contando: <br>'
+        var i = Number(NumInicio.value)
+        var f = Number(NumFinal.value)
+        var p = Number(passo.value) // Para tranformar a string em valor numerico
+
+        if (passo<=0){
+            alert('Passo inválido')
+            p = 1
+        }
+
+
+        //Contagem progressiva
+        if (i < f){
+            for(var c = i; c <= f; c += p){
+            resultado.innerHTML += ` ${c} \u{1F449}`
+        }
+
+        //Contagem regressiva
+        } else {
+            for(var c = i; c >=f; c -= p)
+            resultado.innerHTML += ` ${c} \u{1F449}`
+    }
+        resultado.innerHTML += `\u{1F3C1}`
+    }
+}
